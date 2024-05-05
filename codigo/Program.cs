a9c29e3c9458479620e1d3bba8802270eb5b427e
duanehenrique <74326441+duanehenrique@users.noreply.github.com>
2024-05-05 00:44:34 -0300

    Update Program.cs

--- codigo/Program.cs
+++ codigo/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adicionando o contexto do banco de dados com a string de conexão
+// Adicionando o contexto do banco de dados com a string de conexÃ£o
 builder.Services.AddDbContext<LocadoraContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("LocadoraDeVeiculosConnection")));
 
@@ -21,29 +21,20 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+
+        // Chamada ao mÃ©todo para resetar e repopular o banco de dados
+        SeedData.ResetAndSeedDatabase(app.Services);
+    }
+
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Locadora De Veiculos API V1");
         c.RoutePrefix = "swagger";
     });
-}
-
-// Adiciona um escopo para inicialização de dados
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        var context = services.GetRequiredService<LocadoraContext>();
-        // Inicializa o banco de dados com Seed Data
-        SeedData.Initialize(services);
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "Ocorreu um erro ao popular o banco de dados.");
-    }
 }
 
 // Endpoint para redirecionar para o Swagger UI
